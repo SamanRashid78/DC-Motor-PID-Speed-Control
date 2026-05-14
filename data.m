bf9b41a75387fedc67336f0ca7c09c406afedf69
Saman Rashid <k236078@nu.edu.pk>
2026-05-14 16:22:06 +0500

    Update data.m

--- data.m
+++ data.m
@@ -1,7 +1,7 @@
 %Data
 info = stepinfo(sys_cl);
 
-fprintf('--- PID Performance Metrics ---\n');
+fprintf('PID Performance Metrics\n');
 fprintf('Overshoot:      %.1f%%\n',  info.Overshoot);
 fprintf('Settling time:  %.2f s\n',  info.SettlingTime);
 fprintf('Rise time:      %.2f s\n',  info.RiseTime);
